10aa385048e824d3f96c2956cc7580ec850d5131
maojindao55 <hobby0524@gmail.com>
2025-03-03 11:02:27 +0800

    修复模型添加 name:的问题, 新增AI树洞倾诉群

--- src/config/groups.ts
+++ src/config/groups.ts
@@ -9,7 +9,7 @@ export interface Group {
 
 export const groups: Group[] = [
   {
-    id: 'group2',
+    id: 'group1',
     name: '前端开发交流群',
     description: '可以适当打招呼问候自我介绍，但是本群主线是前端开发交流群，请严格按照前端开发交流规则，不能过度闲聊',
     isGroupDiscussionMode: true,
